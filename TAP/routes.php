6fa510ce43f23f9b3d111a91efe01764b711cf06
MD WALI MOSNAD AYSHIK <Ayshikmee@gmail.com>
2022-06-12 16:51:48 +0600

    refresh

--- TAP/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-// This route is for TAP
-
-Route::group(['prefix' => 'tap','namespace'=>'App\Modules\TAP\Controllers','middleware'=>['web','roles'],'roles'=>['TAP']], function () {
-
-    Route::get('/dashboard','TestController@index')->name('panel');
-	Route::get('/transaction','TestController@transaction')->name('transaction');
-
-});
